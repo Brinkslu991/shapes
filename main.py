8e955303050bd03dad654816ce14a7f9f8c1b166
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2025-02-24 10:48:54 -0500

    made the code

--- main.py
+++ main.py
@@ -10,6 +10,24 @@ def init_game ():
     pygame.display.set_caption(config.TITLE)
     return screen
 
+def draw_rectangle(screen, rect, color, thickness):
+    pygame.draw.rect(screen, color, rect, thickness)
+
+my_rect1 = [100, 100, 200, 150]
+thickness1 = 8   
+
+def draw_circle(screen, center, radius, color, thickness):
+    pygame.draw.circle(screen, color, center, radius, thickness)
+
+circle_center = (500, 200)  
+circle_radius = 50          
+circle_color = config.SUNSETORANGE  
+circle_thickness = 0 
+
+def draw_line(surface, color, start_pos, end_pos, thickness):
+    pygame.draw.line(surface, color, start_pos, end_pos, thickness)
+
+
 def handle_events ():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -25,10 +43,16 @@ def main():
     while running:
         running = handle_events()
         screen.fill(config.WHITE) # Use color from config
-        pygame.display.flip()
+        
         
         # Limit the frame rate to the specified frames per second (FPS)
         clock.tick(config.FPS) # Use the clock to control the frame rate
+
+        draw_rectangle(screen, my_rect1, config.AQUAMARINE, thickness1)
+        draw_circle(screen, circle_center, circle_radius, circle_color, circle_thickness)
+        draw_line(screen, config.FIREBRICK, [200,300], [777,666], 4)
+
+        pygame.display.flip()
 
     pygame.quit()
 
